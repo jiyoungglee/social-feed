2063248d7bee734bea0117dedc38873fed2a3a60
Jiyoung Lee <jiyoung.g.lee118@gmail.com>
2022-09-22 13:48:01 -0400

    Switch to HashRouter for express static routing issue

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import UserContextProvider from './store/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </UserContextProvider>
   </React.StrictMode>
 );
